ea582da565f5db00466f6d47738676009bbe9b75
KAUANSANTOS <66709503+KAUANPEREIRA@users.noreply.github.com>
2021-08-11 12:49:42 -0300

    Update script.js

--- script/script.js
+++ script/script.js
@@ -3,7 +3,7 @@
 // const qs utilizada para pegar algo com document.querySelector
 
 //preventDefault = evita que um evento padrão aconteça exemplo submitar a pagina e.preventDefault()==previna a ação padrão
-
+//closest('procura algo mais proximo do atributo que foi passado')
 const qs = (elemento)=>{
     return document.querySelector(elemento)
 }
@@ -15,12 +15,21 @@ const qsAll =(elemento)=>{
 
 pizzaJson.map((item,index)=>{
  let pizzaItem = qs('.models .pizza-item').cloneNode(true)
+
+ pizzaItem.setAttribute('data-key', index)
  pizzaItem.querySelector('.pizza-item--img img').src = item.img
  pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name
  pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description
  pizzaItem.querySelector('.pizza-item--price').innerHTML = ` R$ ${item.price.toFixed(2)}`
  pizzaItem.querySelector('a').addEventListener('click', (e)=>{
      e.preventDefault()
+     let key = e.target.closest('.pizza-item').getAttribute('data-key')
+     console.log(pizzaJson[key])
+    qs('.pizzaBig img').src =pizzaJson[key].img
+    qs('.pizzaInfo h1').innerHTML = pizzaJson[key].name
+    qs('.pizzaInfo--desc').innerHTML = pizzaJson[key].description
+    qs('.pizzaInfo--actualPrice').innerHTML = ` R$ ${pizzaJson[key].price}`
+
      qs('.pizzaWindowArea').style.opacity=0
      qs('.pizzaWindowArea').style.display='flex'
     setTimeout(()=>{
